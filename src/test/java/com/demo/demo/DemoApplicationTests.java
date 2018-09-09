5ef45eee45c67bc95098f53737b5fd04fb791be8
wubiao <wubiao@weidian.com>
2018-09-09 18:41:11 +0800

    init

--- src/test/java/com/demo/demo/DemoApplicationTests.java
+++ src/test/java/com/demo/demo/DemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.demo.demo;
 
+import lombok.Data;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +12,25 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class DemoApplicationTests {
 
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws Exception {
+        while(true){
+           C1 c1 = new C1();
+           c1.setContent(RandomStringUtils.randomAlphabetic(10000));
+//            System.out.println(c1);
+//            Thread.sleep(500l);
+        }
+
     }
+
+    @Data
+    class C1{
+        private String content;
+    }
+
+    @Data
+    class C2{
+        private String content;
+    }
+
 
 }
---
+++ src/test/java/com/demo/demo/HProfTest.java
@@ -0,0 +1,4 @@
+package com.demo.demo;
+
+public class HProfTest {
+}
